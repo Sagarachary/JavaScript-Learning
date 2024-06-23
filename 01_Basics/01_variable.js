f11d2a769c8fa1a8abbcd04f4c149d41f07136f8
G. Sagar Achary <99972005+Sagarachary@users.noreply.github.com>
2024-06-23 10:13:07 +0000

    Basics in js

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "acharysagar01@gmail.com"
+var accountPassword = "12345"
+accountCity = "Aska"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
