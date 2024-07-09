e6b617c5a815f0ec5552913785fac54cc80b1f82
G. Sagar Achary <99972005+Sagarachary@users.noreply.github.com>
2024-07-09 08:55:04 +0000

    Conversion of The Operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "Sagar"
+
+
+let valueInNumber= Number(score)
+let isLoggedIn ="Sagar"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+let someNumber= 33
+let stringNumber = String(someNumber)
+let value = 3
+let negValue = -value
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
