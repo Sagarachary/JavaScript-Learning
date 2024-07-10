32f19fd8ded619ffc38d734fb3a3e2f7a6616900
G. Sagar Achary <99972005+Sagarachary@users.noreply.github.com>
2024-07-10 09:06:33 +0000

    comparision.js file added

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,8 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log("2" === 2);
