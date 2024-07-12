8171f3e15738143136b1f6b188c9f4b848d3bc74
G. Sagar Achary <99972005+Sagarachary@users.noreply.github.com>
2024-07-12 10:10:06 +0000

    Basic String.js

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Sagar"
+const repoCount = 10
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Sagar-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "Sagar"
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://preeminent-trifle-d23bac.netlify.app"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
