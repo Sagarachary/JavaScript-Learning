4e1088d6f7060efafc1f84a5f27aead1d6952f94
G. Sagar Achary <99972005+Sagarachary@users.noreply.github.com>
2024-07-15 04:37:21 +0000

    math.js

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,16 @@
+const score = 400
+
+const balance = new Number(100)
+
+const otherNumber = 123.8966
+
+const hundreds = 1000000
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
