b0d821ecdd1d76f55c0435f99b5a18856cfa2890
G. Sagar Achary <99972005+Sagarachary@users.noreply.github.com>
2024-07-15 04:43:56 +0000

    datesinjs.js

---
+++ 01_Basics/07_datesinJs.js
@@ -0,0 +1,18 @@
+// Dates
+
+let myDate = new Date()
+
+let myCreatedDate = new Date("01-14-2023")
+
+let myTimeStamp = Date.now()
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
