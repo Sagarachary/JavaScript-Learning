82c62eba4b4ddc0e0ce06855fab36263fd0c827a
G. Sagar Achary <99972005+Sagarachary@users.noreply.github.com>
2024-07-17 09:45:46 +0000

    Array.js

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,18 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
