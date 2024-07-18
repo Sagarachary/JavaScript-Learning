506b04ad0d23cf440762d0f6b0d608c1693c64fc
G. Sagar Achary <99972005+Sagarachary@users.noreply.github.com>
2024-07-18 06:07:19 +0000

    object.js

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,69 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Sagar",
+            lastname: "Achary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "Sagar@gmail.com"
+    },
+    {
+        id: 1,
+        email: "Sagar@gmail.com"
+    },
+    {
+        id: 1,
+        email: "Sagar@gmail.com"
+    },
+]
+
+users[1].email
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Sagar"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+[
+    {},
+    {},
+    {}
+]
