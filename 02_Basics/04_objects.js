ff85bdc8c52dc22c8d73066869ed0bd2eedbc3a5
Sagarachary <acharysagar01@gmail.com>
2024-08-20 00:10:31 +0530

    changes on object.js

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,69 +1,61 @@
 // const tinderUser = new Object()
 const tinderUser = {}
 
-tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
+tinderUser.id = "123abc",
+tinderUser.name = "Sagar",
 tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
+// Declare Object inside object
+
 const regularUser = {
-    email: "some@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "G.Sagar",
-            lastname: "Achary"
+    email: "sagar@gmail.com",
+    fullname:{
+        Userfullname: {
+            firstName: "Sagar",
+            lastName: "Achary"
         }
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.Userfullname.firstName);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
-const obj4 = {5: "a", 6: "b"}
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+const obj4= {5:"e",6:"f"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
-
-const obj3 = {...obj1, ...obj2}
+// const object3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2,obj4)
+const obj3 = {...obj1, ...obj2,...obj4}
 // console.log(obj3);
 
 
-const users = [
+const users =[
     {
-        id: 1,
-        email: "Sagar@gmail.com"
+        id:1,
+        email: "s@example.com"
     },
     {
-        id: 1,
-        email: "Sagar@gmail.com"
+        id:2,
+        email: " john@ example.com"
     },
     {
-        id: 1,
-        email: "Sagar@gmail.com"
-    },
+        id:3,
+        email: " john@example again.com"
+    }
 ]
 
 users[1].email
+console.log(tinderUser);
 
-const course = {
-    coursename: "js in hindi",
-    price: "999",
-    courseInstructor: "Sagar"
-}
 
-// course.courseInstructor
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
-const {courseInstructor: instructor} = course
-
-// console.log(courseInstructor);
-console.log(instructor);
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 
-[
-    {},
-    {},
-    {}
-]
+
