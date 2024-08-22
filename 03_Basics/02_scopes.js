a7ab96bf4d4f14d9a3606d2adccdd277066cc086
Sagarachary <acharysagar01@gmail.com>
2024-08-22 16:37:49 +0530

    changes on  section.js

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -9,6 +9,44 @@ if (true) {
 //     const element = array[i];
 // }
 
-console.log(a);
+// console.log(a);
+
+// Nasted Scope
+function one(){
+    const username = 'Sagar'
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+    
+}
+// one()
+if(true){
+    const username = 'Sagar'
+    if(username === 'Sagar'){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
 
 
+//  ++++++++++++++ Intresting ++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+addone(2)
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
