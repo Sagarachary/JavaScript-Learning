7bdc7a8dfe39b65645fd5ddc1f265abee750fa22
Sagarachary <acharysagar01@gmail.com>
2024-08-22 11:52:29 +0530

    changes on Scope.js

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -3,42 +3,12 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    // console.log("INNER: ", a);
-    
+    // console.log("INNER: ", a);  
 }
+// for(let i = 0; i<Array.length;i++){
+//     const element = array[i];
+// }
 
-function one(){
-    const username = "Sagar"
-
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-   
-     two()
-
-}
-
-// one()
-
-if (true) {
-    const username = "Sagar"
-    if (username === "Sagar") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    
-}
-
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
-}
+console.log(a);
 
 
-
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
-}
