6f9ec276f6bebfb32f805797e7f23483627a871f
Sagarachary <acharysagar01@gmail.com>
2024-08-22 17:14:54 +0530

    changes on arrow.js

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -1,22 +1,48 @@
 const user = {
-    username: "Sagar",
+    username: 'Sagar',
     price: 999,
 
-    welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
+    welcomeMessage: function(){
+        // this keyword refers the current context.
+        console.log(`${this.username}, Welcome to the website`);
         console.log(this);
+        
     }
-
 }
+// user.welcomeMessage()
+// user.username = "Deepak"
+// user.welcomeMessage()
 
+// console.log(this);
 
-const chai =  () => {
+// function chai(){
+//     let username = "Sagar"
+//     console.log(this.username);
+// }
+// chai()
+
+const chai = () => {
     let username = "Sagar"
     console.log(this);
 }
 
 
-const addTwo = (num1, num2) => ({username: "Sagar"})
+// chai()
+// explicit retrurn
+// const addTwo=(num1,num2) => {
+//     return num1+num2
+// }
+
+// implesit return
+// const addTwo=(num1,num2) =>  num1+num2
+
+// const addTwo=(num1,num2) =>  (num1+num2)
+const addTwo=(num1,num2) => ({username: "sagar"})
+
+console.log(addTwo(2,3));
 
 
-console.log(addTwo(3, 4))
+// const myArray = [1,2,3,4,5,6,7,8,9,10]
+// myArray.forEach((element,index) => {
+//     console.log(element);
+// })
