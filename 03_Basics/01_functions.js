691d82d006e4c3aa3e9e64728596bece79218243
Sagarachary <acharysagar01@gmail.com>
2024-08-22 11:40:41 +0530

    changes on function.js

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,59 +1,66 @@
 function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
     console.log("S");
-    console.log("H");
+    console.log("A");
+    console.log("G");
+    console.log("A");
+    console.log("R");
 }
 
 
+// sayMyName() 
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
 
 function addTwoNumbers(number1, number2){
+    // let result = number1 + number2;
+    // return result;
 
-    
-    return number1 + number2
+    return number1 + number2;
 }
+const result = addTwoNumbers(1,2)
+// console.log("Result: " , result);
 
-const result = addTwoNumbers(3, 5)
 
-
-
-function loginUserMessage(username = "Sagar"){
+function loginUserMessage(username = "sam"){
     if(!username){
-        console.log("PLease enter a username");
-        return
+        console.log("Please provide a username");
+        return;
     }
     return `${username} just logged in`
 }
 
+// console.log(loginUserMessage("Sagar"));
 
-
-function calculateCartPrice(val1, val2, ...num1){
+function calculateCartPrice(...num1){  // Rest Operator(...)
     return num1
 }
+// console.log(calculateCartPrice(200,400,600))
 
-
-
-const user = {
-    username: "Sagar",
-    prices: 199
+const user ={
+    username : "Sagar",
+    price: 199
 }
 
 function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
+// handleObject(user);
 
-// handleObject(user)
 handleObject({
-    username: "Sagar",
-    price: 399
+    username : "Sam",
+    price: 299
 })
 
-const myNewArray = [200, 400, 100, 600]
+const myNewArray = [200,400,500,800]
 
 function returnSecondValue(getArray){
     return getArray[1]
 }
+// console.log(returnSecondValue(myNewArray));
 
-console.log(returnSecondValue([200, 400, 500, 1000]));
+console.log(returnSecondValue([200,400,500,800]));
+
+
+
