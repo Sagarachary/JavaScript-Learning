12b06d38902ac71c9da8d99959cebe699e5e009e
Sagarachary <acharysagar01@gmail.com>
2024-08-27 12:12:07 +0530

    changes on one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,17 +1,23 @@
 // if
-const isUserloggedIn = true
-const temperature = 41
+// const isUserlogedIn = true
+// const temperature = 41
+
+// if(temperature<50){
+//     console.log("less than 50");
+// }else{
+//     console.log("greater than 50");
+// }
+// console.log("Execute");
 
 
-const userLoggedIn = true
-const debitCard = true
-const loggedInFromGoogle = false
-const loggedInFromEmail = true
 
-if (userLoggedIn && debitCard && 2==3) {
-    console.log("Allow to buy course");
-}
 
-if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("User logged in");
-}
+// Datatypes
+//<,>, <=,>=, ==, !=, ===, !==
+
+// const score = 200
+// if(200>100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);  
+// }
+// console.log(`User score: ${score}`);
