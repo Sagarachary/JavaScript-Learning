65751052d2218582778809f3dd79b15d8d90d00d
Sagarachary <acharysagar01@gmail.com>
2024-08-27 16:23:36 +0530

    changes on one.js

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -21,3 +21,34 @@
 //     console.log(`User power: ${power}`);  
 // }
 // console.log(`User score: ${score}`);
+
+const balance = 1000
+
+// if(balance>500){
+//     console.log("Test");
+// }
+// else{
+//     console.log("Test2");
+// }
+
+// if(balance>500){
+//     console.log("less than 500"); 
+// }else if(balance<750){
+//     console.log("less than 750");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const userLogedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLogedIn && debitcard){
+    console.log("Allow to buy course");
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User loogedIn");
+}
