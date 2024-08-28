dfe8447379e802024bf3d943f08730d606aed562
Sagarachary <acharysagar01@gmail.com>
2024-08-28 12:31:23 +0530

    Changes on truthy.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,18 +1,30 @@
 const userEmail = []
 
-if (userEmail) {
-    console.log("Got user email");
-} else {
+if(userEmail){
+    console.log("Got User email");   
+}else{
     console.log("Don't have user email");
 }
 
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// How check the array
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
+if(Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
 }
 
+// Nullish Coalescing Operaator (??): null undefined
 
 let val1;
 // val1 = 5 ?? 10
@@ -21,7 +33,6 @@ let val1;
 val1 = null ?? 10 ?? 20
 
 
-
 console.log(val1);
-const iceTeaPrice = 100
-iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+// Terniary Operator
